136efd78f7b61fd6278c43fb396babbd8aaf8da2
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2023-12-31 18:33:11 -0500

    Implement lunatic::error functions
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- lunatic/error/error.go
+++ lunatic/error/error.go
@@ -1,21 +1,46 @@
+// -*- compile-command: "GOOS=wasip1 GOARCH=wasm go test ./..."; -*-
+
 // Package error provides the Go bindings to the lunatic::error API.
 package error
 
+import (
+	"fmt"
+	"unsafe"
+)
 
-// StringSize
+// StringSize returns the size of the string representation of the error `errorID`.
 //
 //go:wasmimport lunatic::error string_size
 //go:noescape
-func StringSize(param int64) (int)
+func StringSize(errorID uint64) uint32
 
-// ToString
-//
 //go:wasmimport lunatic::error to_string
 //go:noescape
-func ToString(param int64 int)
+func to_string(errorID uint64, errorStrPtr unsafe.Pointer)
 
-// Drop
-//
+// ToString returns the string representation of the error.
+func ToString(errorID uint64) string {
+	n := StringSize(errorID)
+	buf := make([]byte, 0, n)
+	to_string(errorID, unsafe.Pointer(&buf[0]))
+	return string(buf)
+}
+
 //go:wasmimport lunatic::error drop
 //go:noescape
-func Drop(param int64)
+func drop(errorID uint64)
+
+// Drop drops the error resource.
+//
+// Errors:
+// * If the error ID doesn't exist.
+func Drop(errorID uint64) (err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("error.drop error: %v", r)
+		}
+	}()
+
+	drop(errorID)
+	return nil
+}
