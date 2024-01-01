86ca96fb9af86053c1a92f2cc65590f9af616af5
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2023-12-31 20:29:33 -0500

    Implement lunatic::metrics functions
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- lunatic/metrics/metrics.go
+++ lunatic/metrics/metrics.go
@@ -3,38 +3,105 @@
 // Package metrics provides the Go bindings to the lunatic::metrics API.
 package metrics
 
-// Counter
-//
+import (
+	"fmt"
+	"unsafe"
+)
+
+type size = uint32
+
 //go:wasmimport lunatic::metrics counter
 //go:noescape
-func Counter(param int int int64)
+func counter(nameStrPtr unsafe.Pointer, nameStrLen size, value uint64)
 
-// IncrementCounter
-//
+// Counter sets a counter.
+func Counter(name string, value uint64) (err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("metrics.counter error: %v", r)
+		}
+	}()
+
+	counter(unsafe.Pointer(&name), size(len(name)), value)
+	return nil
+}
+
 //go:wasmimport lunatic::metrics increment_counter
 //go:noescape
-func IncrementCounter(param int int)
+func increment_counter(nameStrPtr unsafe.Pointer, nameStrLen size)
 
-// Gauge
-//
+// IncrementCounter increments a counter.
+func IncrementCounter(name string) (err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("metrics.increment_counter error: %v", r)
+		}
+	}()
+
+	increment_counter(unsafe.Pointer(&name), size(len(name)))
+	return nil
+}
+
 //go:wasmimport lunatic::metrics gauge
 //go:noescape
-func Gauge(param int int f64)
+func gauge(nameStrPtr unsafe.Pointer, nameStrLen size, value float64)
 
-// IncrementGauge
-//
+// Gauge sets a guage.
+func Gauge(name string, value float64) (err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("metrics.gauge error: %v", r)
+		}
+	}()
+
+	gauge(unsafe.Pointer(&name), size(len(name)), value)
+	return nil
+}
+
 //go:wasmimport lunatic::metrics increment_gauge
 //go:noescape
-func IncrementGauge(param int int f64)
+func increment_gauge(nameStrPtr unsafe.Pointer, nameStrLen size, value float64)
 
-// DecrementGauge
-//
+// IncrementGauge increments a gauge.
+func IncrementGauge(name string, value float64) (err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("metrics.increment_gauge error: %v", r)
+		}
+	}()
+
+	increment_gauge(unsafe.Pointer(&name), size(len(name)), value)
+	return nil
+}
+
 //go:wasmimport lunatic::metrics decrement_gauge
 //go:noescape
-func DecrementGauge(param int int f64)
+func decrement_gauge(nameStrPtr unsafe.Pointer, nameStrLen size, value float64)
 
-// Histogram
-//
+// DecrementGauge decrements a gauge.
+func DecrementGauge(name string, value float64) (err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("metrics.decrement_gauge error: %v", r)
+		}
+	}()
+
+	decrement_gauge(unsafe.Pointer(&name), size(len(name)), value)
+	return nil
+}
+
 //go:wasmimport lunatic::metrics histogram
 //go:noescape
-func Histogram(param int int f64)
+func histogram(nameStrPtr unsafe.Pointer, nameStrLen size, value float64)
+
+// Histogram sets a histogram.
+func Histogram(name string, value float64) (err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("metrics.histogram error: %v", r)
+		}
+	}()
+
+	histogram(unsafe.Pointer(&name), size(len(name)), value)
+	return nil
+}
