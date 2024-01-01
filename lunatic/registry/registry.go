12aa319c69975dd288456e032ed29e67be9ccd98
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2023-12-31 20:20:40 -0500

    Implement lunatic::registry functions
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- lunatic/registry/registry.go
+++ lunatic/registry/registry.go
@@ -3,20 +3,57 @@
 // Package registry provides the Go bindings to the lunatic::registry API.
 package registry
 
-// Put
-//
+import (
+	"fmt"
+	"unsafe"
+)
+
+type size = uint32
+
 //go:wasmimport lunatic::registry put
 //go:noescape
-func Put(param int int int64 int64)
+func put(nameStrPtr unsafe.Pointer, nameStrLen size, nodeID, processID uint64)
 
-// Get
-//
+// Put registers process with `processID` under `name`.
+func Put(name string, nodeID, processID uint64) (err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("registry.put error: %v", r)
+		}
+	}()
+
+	put(unsafe.Pointer(&name), size(len(name)), nodeID, processID)
+	return nil
+}
+
 //go:wasmimport lunatic::registry get
 //go:noescape
-func Get(param int int int int) (int)
+func get(nameStrPtr unsafe.Pointer, nameStrLen size, nodeID, processID uint64) uint32
 
-// Remove
-//
+// Get looks up process under `name` and returns if it was found.
+func Get(name string, nodeID, processID uint64) (ok bool, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("registry.get error: %v", r)
+		}
+	}()
+
+	n := get(unsafe.Pointer(&name), size(len(name)), nodeID, processID)
+	return n == 0, nil
+}
+
 //go:wasmimport lunatic::registry remove
 //go:noescape
-func Remove(param int int)
+func remove(nameStrPtr unsafe.Pointer, nameStrLen size)
+
+// Remove removes the process under `name` if it exists.
+func Remove(name string) (err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("registry.remove error: %v", r)
+		}
+	}()
+
+	remove(unsafe.Pointer(&name), size(len(name)))
+	return nil
+}
