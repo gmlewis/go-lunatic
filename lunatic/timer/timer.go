a0703c28747c20433f07ecc8e7f3ec605c3ee129
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2023-12-31 20:02:28 -0500

    Implement lunatic::timer functions
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- lunatic/timer/timer.go
+++ lunatic/timer/timer.go
@@ -3,14 +3,43 @@
 // Package timer provides the Go bindings to the lunatic::timer API.
 package timer
 
-// SendAfter
-//
+import "fmt"
+
 //go:wasmimport lunatic::timer send_after
 //go:noescape
-func SendAfter(param int64 int64) (int64)
+func send_after(processID uint64, delayMillis uint64) uint64
 
-// CancelTimer
+// SendAfter sends the message to a process after a delay.
 //
+// There are no guarantees that the message will be received.
+//
+// Returns:
+// * nil if successful with message ID.
+// * error if the processID doesn't exist or if called before creating the next message.
+func SendAfter(processID uint64, delayMillis uint64) (id uint64, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("timer.send_after error: %v", r)
+		}
+	}()
+
+	id = send_after(processID, delayMillis)
+	return id, nil
+}
+
 //go:wasmimport lunatic::timer cancel_timer
 //go:noescape
-func CancelTimer(param int64) (int)
+func cancel_timer(timerID uint64) uint32
+
+// CancelTimer cancels the specified timer.
+//
+// Returns:
+// * true if the `timerID` was found.
+// * false if no timer was found with `timerID`, because:
+//   - timer had expired
+//   - timer already had been canceled
+//   - timerID never corresponded to a timer.
+func CancelTimer(timerID uint64) bool {
+	n := cancel_timer(timerID)
+	return n == 1
+}
